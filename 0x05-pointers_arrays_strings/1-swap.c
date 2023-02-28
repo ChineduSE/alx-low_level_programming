2314fac41972e7cefdf7c7830ba49b97f2f14822
ChineduSE <chinedunweke618@gmail.com>
2023-02-27 17:26:41 -0800

    arrays

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* swap_int - swaps the value of int a and int b
+* Owned by Chi_Soft_Tech
+* @a: first int
+* @b: second int
+* Return 0
+*/
+void swap_int(int *a, int *b)
+{
+	*a = *b;
+}
