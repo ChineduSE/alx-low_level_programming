c1eeec5a5605d5351e8c53f5571ea211c7a58011
ChineduSE <chinedunweke618@gmail.com>
2023-04-23 16:55:32 -0700

    bit man

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/*
+ * get_endianness- checks for huge or small machine
+ * Return: 0 for huge, 1 for small
+ */
+int get_endianness(void)
+{
+	unsigned int i = 1;
+	char *c = (char *) &i;
+
+	return (*c);
+}
